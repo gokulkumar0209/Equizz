36368d7d56634e95c098faeb543cec03e5f905d6
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-07-14 23:20:07 +0530

    Created an actual state

--- equizz/src/components/Home.jsx
+++ equizz/src/components/Home.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import cont from "../data/data";
 import Template from "./Template";
 function Home() {
+	const map = new Map();
+	const [actual, setActual] = useState(map);
 	console.log(cont);
 	return (
 		<div className=" bg-blue-200">
 			<form>
 				<div className="flex justify-end mr-10">
-				<button type="submit" >Submit</button>
+					<button type="submit">Submit</button>
 				</div>
 				<div className="">
 					{cont.map((q) => {
-						return <Template question={q.question} answers={q.ans} />;
+						return <Template question={q.question} answers={q.ans} actual={actual} setActual={setActual}/>;
 					})}
 				</div>
 			</form>
--- equizz/src/components/Template.jsx
+++ equizz/src/components/Template.jsx
@@ -1,23 +1,58 @@
 import React from "react";
 
-function Template({ question, answers }) {
-	console.log(question);
+
+function Template({ question, answers, actual, setActual }) {
+	const addAns = (index) => {
+		const old = actual;
+		old.set(question, answers[index]);
+		setActual(old);
+		console.log(actual);
+	};
+	console.log(actual);
 	return (
 		<div>
 			<div className="text-xl placeholder:font-bold ml-8 mb-2">
 				<h1>{question}</h1>
 			</div>
 			<div className=" ml-20 mx-20">
-				{answers.map((ans) => {
-					return (
-						<div>
-							<label>
-								<input type="radio"></input>
-								{ans}
-							</label>
-						</div>
-					);
-				})}
+				<label>
+					<input
+						type="radio"
+						name="answers"
+						value={answers[0]}
+						onChange={() => addAns(0)}
+					></input>
+					{answers[0]}
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="answers"
+						value={answers[1]}
+						onChange={() => addAns(1)}
+					></input>
+					{answers[1]}
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="answers"
+						value={answers[2]}
+						onChange={() => addAns(2)}
+						
+					></input>
+					{answers[2]}
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="answers"
+						value={answers[3]}
+						onChange={() => addAns(3)}
+						checked={actual.get(question) === answers[3]}
+					></input>
+					{answers[3]}
+				</label>
 			</div>
 		</div>
 	);
