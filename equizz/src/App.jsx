02937bf52133c769d38b94651321fb2a7702d5b7
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-07-19 22:51:33 +0530

    Just created Login and Signup page

--- equizz/src/App.jsx
+++ equizz/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Home from "./components/Home";
-
+import { Route, BrowserRouter } from "react-router-dom";
+import Navigation from "./components/Navigation";
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div className="bg-blue-200" >
+			<div className="bg-blue-200">
+				<Navigation />
 				<Home />
 			</div>
 		</>
---
+++ equizz/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+	return (
+		<div className=" bg-blue-300">
+			<div >Navigation</div>
+		</div>
+	);
+}
+
+export default Navigation;
